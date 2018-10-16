8444d5fd24a964b7eeda47ea3819f88e559ba5c5
dsmcclain <dylan.mcclain@gmail.com>
2018-10-15 22:39:47 -0700

    import compose as fallback for redux

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
 import { routerMiddleware, routerReducer as routing } from 'react-router-redux';
 
 export const rootReducer = combineReducers({ routing });
