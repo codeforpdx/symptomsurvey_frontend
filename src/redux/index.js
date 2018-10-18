59d04a879a79294529c38465f9c77bd065ad6865
GerryFudd <davehasen@gmail.com>
2018-10-17 21:54:45 -0700

    added redux-localstorage to todos

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,10 +2,17 @@ import {
   applyMiddleware, createStore, combineReducers, compose,
 } from 'redux';
 import { routerMiddleware, routerReducer as routing } from 'react-router-redux';
+import _ from 'lodash';
+import persistState from 'redux-localstorage';
 
 import todo from './reducers/todo/reducer';
 
 export const rootReducer = combineReducers({ routing, todo });
+
+const slicer = paths => (state) => {
+  console.log(state);
+  return _.pick(state, paths);
+};
 
 const middlewares = history => [
   routerMiddleware(history),
@@ -18,5 +25,6 @@ export default (history, initialState) => createStore(
   initialState,
   composeEnhancers(
     applyMiddleware(...middlewares(history)),
+    persistState(['todo.todos'], { slicer }),
   ),
 );
