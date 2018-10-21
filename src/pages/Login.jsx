a31672de2543e77450dce8999ea179af529bb477
dsmcclain <dylan.mcclain@gmail.com>
2018-10-21 13:57:53 -0700

    add state changes, add handleSubmit method, fix errors

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import glamorous from 'glamorous';
 
 const LOGINdiv = glamorous.div({
+    '& > .login-field': {
+    },
     '& > .submit-button': {
         background: '#337ab7',
         color: '#fff',
@@ -27,10 +29,16 @@ class LoginPage extends Component {
             password: '',
             submitted: false
         };
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit() {
+        alert(`You have submitted username '${this.state.username}'' and password '${this.state.password}'`);
     }
 
     render() {
-        const { username, password, submitted } = this.props;
+        const { username, password, submitted } = this.state;
         return (
             <LOGINdiv>
                 <div className={'login-field' + 
@@ -40,26 +48,24 @@ class LoginPage extends Component {
                         type="text" 
                         name="username"
                         value={username} 
-                        onInput={/* setState */}
+                        onInput={e => this.setState({ username: e.target.value }) }
                         />
                 </div>
                 <div className={'login-field' + 
                 (submitted && !password ? 'has-error' : '')}>
                     <label htmlFor="password">Password</label>
                     <input 
-                        type="text" 
+                        type="password" 
                         name="password"
                         value={password} 
-                        onInput={/* setState */}
+                        onInput={e => this.setState({ password: e.target.value }) }
                         />
                 </div>
                 <button
                     className={`submit-button${!(password && username) ? ' disabled' : ''}`}
                     type="button"
                     disabled={!password || !username}
-                    onClick={() => {
-                        //Trigger submit event for reducer
-                    }}
+                    onClick={this.handleSubmit}
                     >
                     submit
                 </button>
@@ -68,4 +74,4 @@ class LoginPage extends Component {
     }
 }
 
-export {LoginPage}
+export default LoginPage;
