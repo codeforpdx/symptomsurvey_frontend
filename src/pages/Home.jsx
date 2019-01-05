535cdaa95a0fe89d22d01aeae5a9237a4f400dc0
Colin Rupp <colin@thinkful.com>
2019-01-04 20:21:49 -0800

    begins to add functionality to form buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
 
 export default class HomeLayout extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerms: '',
+      location: '',
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSearchTerm = this.handleSearchTerm.bind(this)
+    this.handleLocation = this.handleLocation.bind(this)
+  }
+
+  handleSearchTerm(e) {
+    this.setState({searchTerms: e.target.value})
+  }
+
+  handleLocation(e) {
+    this.setState({location: e.target.value})
+  }
 
   handleSubmit() {
-    //TODO submit form data
-    console.log('submit was clicked')
+    event.preventDefault();
+
+    console.log(this.state.searchTerm)
   }
 
   render() {
     return (
       <div>
         <h3>Hello world</h3>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>Include these words or phrases</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTerm}
+          />
           <br/>
           <label>location (optional)</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={this.state.location}
+            onChange={this.handleLocation}
+          />
           <br/>
           <label>radius</label>
           <input type="text"></input>
