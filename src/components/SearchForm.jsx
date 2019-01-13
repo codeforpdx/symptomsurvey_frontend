45e961813cdd26820edf399e2b3fcf2a0dffd830
Colin Rupp <colin@thinkful.com>
2019-01-12 16:59:09 -0800

    recreates the original form using formik

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,105 +1,62 @@
 import React from 'react';
+import { Formik, Field, Form, withFormik } from 'formik';
+
+const SearchForm = ({
+  values,
+  handleChange,
+  handleSubmit
+}) => (
+  <form
+    onSubmit={handleSubmit}
+  >
+    <input
+      type="text"
+      name="searchTerms"
+      placeholder="seperate search terms with a comma (,)"
+      value={values.searchTerms}
+      onChange={handleChange}
+    />
+    <input
+      type="text"
+      name="location"
+      placeholder="97015"
+      value={values.location}
+      onChange={handleChange}
+    />
+    <input
+      type="text"
+      name="radius"
+      placeholder="25"
+      value={values.radius}
+      onChange={handleChange}
+    />
+    <input
+      type="text"
+      name="timeFrame"
+      placeholder="1 month"
+      value={values.timeFrame}
+      onChange={handleChange}
+    />
+    <button>Submit</button>
+  </form>
+)
 
 
-export default class SearchFrom extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchTerms: '',
-      location: '',
-      radius: '',
-      timeframe: '',
+export const FormikSearch = withFormik({
+  mapPropsToValues({
+    searchTerms,
+    location,
+    radius,
+    timeFrame
+  }) {
+    {/* return an object which sets initial values of the form */}
+    return {
+      searchTerms: ''
     }
-
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleSearchTerms = this.handleSearchTerms.bind(this)
-    this.handleLocation = this.handleLocation.bind(this)
-    this.handleRadius = this.handleRadius.bind(this);
-    this.handleTimeframe = this.handleTimeframe.bind(this)
+  },
+  handleSubmit(values) {
+    {/* this is where we would run dispatch or make an AJAX call */}
+    console.log(values)
   }
-
-  handleSearchTerms(e) {
-    this.setState({searchTerms: e.target.value})
-  }
-
-  handleLocation(e) {
-    this.setState({location: e.target.value})
-  }
-
-  handleRadius(e) {
-    this.setState({radius: e.target.value})
-  }
-  handleTimeframe(e) {
-    this.setState({timeframe: e.target.value})
-  }
-
-  handleSubmit() {
-    event.preventDefault();
-    console.log(this.state)
-    this.setState({
-      searchTerms: '',
-      location: '',
-      radius: '',
-      timeframe: '',
-    })
-  }
-
-  render() {
-    {/* validation for form fields is HERE!*/}
-    const {searchTerms, location, radius, timeframe} = this.state;
-      {/* validators for "search" button*/}
-    const searchEnabled =
-      searchTerms.length > 0 &&
-
-    {/* jsx incoming...*/}
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Include these words or phrases</label>
-          <input
-            type="text"
-            value={this.state.searchTerms}
-            onChange={this.handleSearchTerms}
-          />
-          <br/>
-          <label>location (optional, zipcode)</label>
-          <input
-            type="text"
-            value={this.state.location}
-            onChange={this.handleLocation}
-          />
-          <br/>
-          <label>radius (in miles)</label>
-          <input
-            type="text"
-            value={this.state.radius}
-            onChange={this.handleRadius}
-          ></input>
-          <br/>
-          <label>Timeframe (optional)</label>
-          <input
-            type="text"
-            value={this.state.timeframe}
-            onChange={this.handleTimeframe}
-          />
-          <br/>
-          <button type="button">Clear</button>
-          <br/>
-          <button
-            type="button"
-            disabled={!searchEnabled}
-            onClick={() => this.handleSubmit()}>Search</button>
-          <br/>
-          <button type="button">Save Search</button>
-          <br/>
-        </form>
-      </div>
-      )
-  }
-}
-
-
-
-
+})(SearchForm)
 
