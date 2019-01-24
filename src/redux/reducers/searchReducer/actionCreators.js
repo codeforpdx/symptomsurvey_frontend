f214cd103338820c516ec07da97bf2fc587cadd6
Colin Rupp <colin@thinkful.com>
2019-01-24 15:39:09 -0800

    attempts to write reducer for search form... its still in progress

--- src/redux/reducers/searchReducer/actionCreators.js
+++ src/redux/reducers/searchReducer/actionCreators.js
@@ -2,13 +2,32 @@ import actions from './actions.js';
 
 const { REQUEST, SUCCESS, ERROR } = actions;
 
-export const searchFormSubmit = (data) => ({
-  type: SUBMIT_FORM,
-  payload: {
-    request: {
-      url: '/search',
-      method: 'get',
-      data: data,
-    }
-  },
+/* searchQuery is the entire object passed from the form.
+ * {location, radius, savedSearch, searchTerms, timeFrame} */
+export const searchRequest = () => ({
+  type: REQUEST,
 })
+
+/* expect hits to be an array returned by the API */
+export const searchSuccess = (hits) => ({
+  type: SUCCESS,
+  payload:  hits
+})
+
+/* expect error object to be returned by the server */
+export const searchError = (error) => ({
+  type: ERROR,
+  payload: error
+})
+
+export const searchFormSubmit = (searchQuery) => (dispatch, getState) => {
+  dispatch(searchRequest());
+  return fetch(`${API_BASE_URL}/endpoint`, {
+    method: '',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(searchQuery)
+  })
+
+}
--- src/redux/reducers/searchReducer/index.js
+++ src/redux/reducers/searchReducer/index.js
@@ -1,17 +1,30 @@
 import actions from './actions.js';
 
-const { SUBMIT_FORM } = actions;
+const { REQUEST, SUCCESS, ERROR } = actions;
 
 const initialState = {
   loading: false,
-  error: null
+  error: null,
+  /* hits is the list of items returned by the API after searching*/
+  hits: []
 }
 
 export default (state = initialState, action) => {
   const { type, payload } = actions;
   switch (type) {
-    case `${SUBMIT_FORM}_REQUEST`:
-      return {...state, loading: true, error: null}
+    case REQUEST:
+      return {...state,
+        loading: true,
+        error: null,
+      }
+    case SUCCESS:
+      return {...state,
+        loading: false,
+      }
+    case ERROR:
+      return {...state,
+
+      }
     default:
       return state;
   }
