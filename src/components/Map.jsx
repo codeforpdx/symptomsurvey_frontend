159c8fbc184644b522d008ca18b30c47e4793d55
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-04-04 15:56:56 -0700

    basic map setup

---
+++ src/components/Map.jsx
@@ -0,0 +1,56 @@
+// AIzaSyAWRJnX0ewu1gSCusxK18jbVSmw8xBQGtI
+// lat: 45.516, lng: -122.679
+
+import React from "react"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAWRJnX0ewu1gSCusxK18jbVSmw8xBQGtI&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: 45.516, lng: -122.679 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 45.516, lng: -122.679 }} onClick={props.onMarkerClick} />}
+  </GoogleMap>
+)
+
+class MyFancyComponent extends React.PureComponent {
+  state = {
+    isMarkerShown: false,
+  }
+
+  componentDidMount() {
+    this.delayedShowMarker()
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false })
+    this.delayedShowMarker()
+  }
+
+  render() {
+    return (
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    )
+  }
+}
+
+export default MyFancyComponent;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import FormikSearch from '../components/SearchForm.jsx';
 import TweetsTable from '../components/TweetsTable.jsx';
+import Map from '../components/Map.jsx';
 
 export default function HomeLayout() {
 
     return (
       <div>
         <FormikSearch/>
+        <Map/>
         <TweetsTable/>
       </div>
       )
