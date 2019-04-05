395a8533394fbd91e718fa7af5fdb8e9204047fa
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-04-05 13:58:56 -0700

    use map clusterer to display fakeData

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,15 @@
 // AIzaSyAWRJnX0ewu1gSCusxK18jbVSmw8xBQGtI
 // lat: 45.516, lng: -122.679
 
-import React from "react"
-import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import React from "react";
+import fetch from "isomorphic-fetch";
+import { compose, withProps, withHandlers } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { MarkerClusterer } from "react-google-maps/lib/components/addons/MarkerClusterer";
 
-const MyMapComponent = compose(
+import fakeData from "../fakeData.json";
+
+const MapWithAMarkerClusterer = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAWRJnX0ewu1gSCusxK18jbVSmw8xBQGtI&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
@@ -19,38 +23,68 @@ const MyMapComponent = compose(
     defaultZoom={8}
     defaultCenter={{ lat: 45.516, lng: -122.679 }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 45.516, lng: -122.679 }} onClick={props.onMarkerClick} />}
-  </GoogleMap>
+  <MarkerClusterer
+        onClick={props.onMarkerClustererClick}
+        averageCenter
+        enableRetinaIcons
+        gridSize={60}
+      >
+        {props.markers.map(marker => (
+          <Marker
+            key={marker.id}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            label={marker.user}
+          />
+        ))}
+      </MarkerClusterer>
+    </GoogleMap>
 )
 
-class MyFancyComponent extends React.PureComponent {
-  state = {
-    isMarkerShown: false,
+class MapClusterer extends React.PureComponent {
+  componentWillMount() {
+    this.setState({
+      markers: []
+    })
   }
 
   componentDidMount() {
-    this.delayedShowMarker()
-  }
+    function convertTwitterToMarkers(data) {
+      return data.filter(obj => obj.geo).map(obj =>
+        ({
+          id: obj.id,
+          user: obj.user.screen_name,
+          lat: obj.geo.coordinates[0],
+          lng: obj.geo.coordinates[1]
+        })
+      )
+    }
 
-  delayedShowMarker = () => {
-    setTimeout(() => {
-      this.setState({ isMarkerShown: true })
-    }, 3000)
-  }
+    this.setState({
+      markers: convertTwitterToMarkers(fakeData)
+    });
 
-  handleMarkerClick = () => {
-    this.setState({ isMarkerShown: false })
-    this.delayedShowMarker()
+    // ### DEFAULT METHOD from react-google-maps docs
+    // ### use this when importing real data in the future?
+
+    // const url = [
+    //   // Length issue
+    //   `https://gist.githubusercontent.com`,
+    //   `/farrrr/dfda7dd7fccfec5474d3`,
+    //   `/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json`
+    // ].join("")
+    //
+    // fetch(url)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     this.setState({ markers: data.photos });
+    //   });
   }
 
   render() {
     return (
-      <MyMapComponent
-        isMarkerShown={this.state.isMarkerShown}
-        onMarkerClick={this.handleMarkerClick}
-      />
+      <MapWithAMarkerClusterer markers={this.state.markers} />
     )
   }
 }
 
-export default MyFancyComponent;
+export default MapClusterer;
