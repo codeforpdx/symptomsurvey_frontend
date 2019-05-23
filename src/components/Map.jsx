892be67930d7ba76657399853f501787f9af3891
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-05-22 19:03:10 -0700

    remove react-google-maps's default fetch

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -63,22 +63,6 @@ class MapClusterer extends React.PureComponent {
     this.setState({
       markers: convertTwitterToMarkers(fakeData)
     });
-
-    // ### DEFAULT METHOD from react-google-maps docs
-    // ### use this when importing real data in the future?
-
-    // const url = [
-    //   // Length issue
-    //   `https://gist.githubusercontent.com`,
-    //   `/farrrr/dfda7dd7fccfec5474d3`,
-    //   `/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json`
-    // ].join("")
-    //
-    // fetch(url)
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     this.setState({ markers: data.photos });
-    //   });
   }
 
   render() {
